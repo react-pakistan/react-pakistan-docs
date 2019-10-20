c968ee640dd8d12882cde37315592584c16577ba
Taimoor Khan <taimoor.muhammad@arbisoft.com>
2019-10-20 11:33:47 +0500

    :recycle: updated footer doc links

--- website/core/Footer.js
+++ website/core/Footer.js
@@ -37,14 +37,20 @@ class Footer extends React.Component {
           </a>
           <div>
             <h5>Docs</h5>
-            <a href={this.docUrl('doc1.html', this.props.language)}>
-              Getting Started (or other categories)
+            <a href={this.docUrl('react-commons-collection.html', this.props.language)}>
+              React Commons Collection
             </a>
-            <a href={this.docUrl('doc2.html', this.props.language)}>
-              Guides (or other categories)
+            <a href={this.docUrl('react-ui-collection.html', this.props.language)}>
+              React UI Collection
             </a>
-            <a href={this.docUrl('doc3.html', this.props.language)}>
-              API Reference (or other categories)
+            <a href={this.docUrl('react-icon-collection.html', this.props.language)}>
+              React Icon Collection
+            </a>
+            <a href={this.docUrl('react-logo-collection.html', this.props.language)}>
+              React Logo Collection
+            </a>
+            <a href={this.docUrl('react-native-boilerplate.html', this.props.language)}>
+              React Native Boilerplate
             </a>
           </div>
           <div>
