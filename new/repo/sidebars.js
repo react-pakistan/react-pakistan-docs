4c73229eff53cf1e5478b48936f20941a62baeed
Taimoor Khan <taimoor.muhammad@arbisoft.com>
2020-10-06 04:56:27 +0500

    :recycle: updated docs

--- new/repo/sidebars.js
+++ new/repo/sidebars.js
@@ -1,13 +1,12 @@
 module.exports = {
   someSidebar: {
-    ReactCommons: ["react-commons-collection", "react-commons-contributors"],
-    ReactUI: ["react-ui-collection", "react-ui-contributors"],
-    ReactIcon: ["react-icon-collection", "react-icon-contributors"],
-    ReactLogo: ["react-logo-collection", "react-logo-contributors"],
-    ReactEmoji: ["react-emoji-collection", "react-emoji-contributors"],
-    ReactNative: ["react-native-boilerplate", "react-native-contributors"],
-    ReactDocs: ["react-docs-contributors"],
-    Features: ['mdx'],
-    StyleGuide: ['doc1', 'doc2', 'doc3']
+    Intro: ['intro'],
+    ReactCommons: ['react-commons-collection', 'react-commons-contributors'],
+    ReactUI: ['react-ui-collection', 'react-ui-contributors'],
+    ReactIcon: ['react-icon-collection', 'react-icon-contributors'],
+    ReactLogo: ['react-logo-collection', 'react-logo-contributors'],
+    ReactEmoji: ['react-emoji-collection', 'react-emoji-contributors'],
+    ReactNative: ['react-native-boilerplate', 'react-native-contributors'],
+    ReactDocs: ['react-pakistan-docs-contributors'],
   },
 };
