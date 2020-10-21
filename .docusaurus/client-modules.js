7ab3f63b1ce84bf9db15c8ffb08634a4617e500e
Taimoor Khan <taimoor.muhammad@arbisoft.com>
2020-10-21 14:00:50 +0500

    :recycle: removed docusaurus folder

--- .docusaurus/client-modules.js
+++
@@ -1,9 +0,0 @@
-export default [
-  require("/Users/taimoor.khan/programming/own/react-pakistan/react-pakistan-docs/node_modules/remark-admonitions/styles/infima.css"),
-  require("/Users/taimoor.khan/programming/own/react-pakistan/react-pakistan-docs/node_modules/remark-admonitions/styles/infima.css"),
-  require("/Users/taimoor.khan/programming/own/react-pakistan/react-pakistan-docs/node_modules/remark-admonitions/styles/infima.css"),
-  require("/Users/taimoor.khan/programming/own/react-pakistan/react-pakistan-docs/node_modules/@docusaurus/plugin-google-analytics/src/analytics"),
-  require("/Users/taimoor.khan/programming/own/react-pakistan/react-pakistan-docs/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/taimoor.khan/programming/own/react-pakistan/react-pakistan-docs/node_modules/@docusaurus/theme-classic/src/prism-include-languages"),
-  require("/Users/taimoor.khan/programming/own/react-pakistan/react-pakistan-docs/src/css/custom.css"),
-];
